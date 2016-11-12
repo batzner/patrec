ac71271550b425a9c6a22e5cfb772544721849c6
batzner <github@kilians.net>
2016-11-12 13:57:42 +0100

    Renamed FlaskWebProject1 to src

--- src/views.py
+++ src/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from FlaskWebProject1 import app
+from src import app
 
 @app.route('/')
 @app.route('/home')

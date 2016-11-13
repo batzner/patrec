335d0ccdf19e3b932f0ad67cf13550cdeed2cf99
batzner <github@kilians.net>
2016-11-13 21:11:52 +0100

    Styled result.html

--- src/static/scripts/result.js
+++ src/static/scripts/result.js
@@ -1,5 +1,31 @@
 $('.alert').hide();
 
+// Display the spinner
+var opts = {
+  lines: 14 // The number of lines to draw
+, length: 15 // The length of each line
+, width: 5 // The line thickness
+, radius: 30 // The radius of the inner circle
+, scale: 1 // Scales overall size of the spinner
+, corners: 0 // Corner roundness (0..1)
+, color: '#c8cbd7' // #rgb or #rrggbb or array of colors
+, opacity: 0.25 // Opacity of the lines
+, rotate: 0 // The rotation offset
+, direction: 1 // 1: clockwise, -1: counterclockwise
+, speed: 1 // Rounds per second
+, trail: 60 // Afterglow percentage
+, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+, zIndex: 2e9 // The z-index (defaults to 2000000000)
+, className: 'spinner' // The CSS class to assign to the spinner
+, top: '50%' // Top position relative to parent
+, left: '50%' // Left position relative to parent
+, shadow: false // Whether to render a shadow
+, hwaccel: false // Whether to use hardware acceleration
+, position: 'absolute' // Element positioning
+}
+var target = document.getElementById('spinner')
+var spinner = new Spinner(opts).spin(target);
+
 // Load the matches
 $.get('/api/find-matches')
     .done(function(data) {
@@ -14,11 +40,12 @@ function fillMatches(data) {
     var resultList = $('#result-list');
     resultList.empty();
     data.forEach(function (match) {
-        var resultItem = $('<li class="result-item"/>');
-        resultItem.append($('<img src="/api/get-image/'+match.id+'"/>'));
+        var resultItem = $('<div class="result-item box"/>');
+        resultItem.append($('<div class="img-wrapper"><img src="/api/get-image/'+match.id+'"/></div>'));
         var score = parseInt(match.value*100);
         var isMatch = match.is_match ? 'yes' : 'no';
-        resultItem.append($('<span>Score: '+score+'% Match: '+isMatch+'</span>'));
+        resultItem.append($('<p>Score: '+score+'%</p>'));
+        resultItem.append($('<p>Match: '+isMatch+'</p>'));
         resultList.append(resultItem);
     });
 }

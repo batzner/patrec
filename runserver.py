386c243dbe17f1691a44f7dd13583a4d114822ba
Felix Schober <Felix Schober>
2016-11-26 23:53:04 +0100

    Added Dockerfile, allowed server to listen to 0.0.0.0 and changed port to 80.

--- runserver.py
+++ runserver.py
@@ -5,4 +5,4 @@ This script runs the application using a development server.
 from src import app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
